6a7b356e84fedf8f37939f8637293657b1ecac11
Eric Watyekele <ewatyeks@gmail.com>
2025-08-04 12:59:57 +0300

    Remove redundant CORS headers from central-db API endpoint

--- api/central-db.js
+++ api/central-db.js
@@ -157,16 +157,6 @@ function initCentralDb() {
 initCentralDb();
 
 export default function handler(req, res) {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
   // Verify authentication
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
